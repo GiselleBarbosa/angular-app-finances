d64f8731b73d51ded446405f5111db8f31313975
TiagoBarbosa88 <sombr43@gmail.com>
2023-02-05 20:19:50 -0300

    create transaction component

--- src/app/components/pages/transaction/transaction.component.ts
+++ src/app/components/pages/transaction/transaction.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-transaction',
   templateUrl: './transaction.component.html',
   styleUrls: ['./transaction.component.css']
 })
 export class TransactionComponent implements OnInit {
+incomes:number = 0;
+expense:number = 0;
+total:number = 0;
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit() {
+    }
+
+
+
 
 }
