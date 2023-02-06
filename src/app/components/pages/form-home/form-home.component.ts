6536a5c814558240228d7e6737834bbf74a15e8e
GiselleBarbosa <g88barbosa@gmail.com>
2023-02-06 13:53:52 -0300

    setting validation form

--- src/app/components/pages/form-home/form-home.component.ts
+++ src/app/components/pages/form-home/form-home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form-home',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormHomeComponent implements OnInit {
 
+  formulario = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.min(3)]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    passwordConfirm: new FormControl('', [Validators.required, Validators.minLength(6)]),
+  });
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  catchInputValues() {
+    let name = this.formulario.controls.name.value;
+    let email = this.formulario.controls.email.value;
+    let password = this.formulario.controls.password.value;
+    let passwordConfirm = this.formulario.controls.passwordConfirm.value;
+  }
+
+  onSubmit() {
+    if (this.formulario.valid) {
+      console.log(this.formulario.value);
+      console.log("ENVIADO COM SUCESSO");
+    }
+    else {
+      console.log("formulario INVALIDO");
+    }
+  }
 }
