324831be5001dc46e9bb5d1076441019ab9501f2
GiselleBarbosa <g88barbosa@gmail.com>
2023-02-09 22:40:33 -0300

    chore: add form component

---
+++ src/app/transaction/transaction-form/transaction-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-transaction-form',
+  templateUrl: './transaction-form.component.html',
+  styleUrls: ['./transaction-form.component.scss']
+})
+export class TransactionFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/transaction/transaction.module.ts
+++ src/app/transaction/transaction.module.ts
@@ -6,12 +6,14 @@ import { MaterialModule } from '../shared/material.module';
 import { TransactionComponent } from './transaction.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { TransactionTableComponent } from './transaction-table/transaction-table.component';
+import { TransactionFormComponent } from './transaction-form/transaction-form.component';
 
 
 @NgModule({
   declarations: [
     TransactionComponent,
-    TransactionTableComponent
+    TransactionTableComponent,
+    TransactionFormComponent
   ],
   imports: [
     CommonModule,
