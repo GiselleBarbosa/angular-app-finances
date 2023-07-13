b2a119b1120b41da37defaa11713e78a778377d0
Giselle Barbosa <g88barbosa@gmail.com>
2023-07-13 20:09:02 -0300

    chore: removed unused imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,14 @@
 import { HttpClientModule } from '@angular/common/http';
 import { LOCALE_ID, NgModule } from '@angular/core';
-import { AngularFireModule } from '@angular/fire/compat';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from 'src/app/app-routing.module';
 
 import { AppComponent } from './app.component';
-import { FooterComponent } from './core/components/footer/footer.component';
-import { HeaderComponent } from './core/components/header/header.component';
-import { LoginModalComponent } from './core/components/login-modal/login-modal.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { HeaderComponent } from './core/header/header.component';
+import { LoginModalComponent } from './core/login-modal/login-modal.component';
 import { SharedModule } from './shared/shared.module';
 
 import { registerLocaleData } from '@angular/common';
@@ -33,7 +32,7 @@ registerLocaleData(ptBr);
     HttpClientModule,
     SharedModule,
   ],
-  exports: [AngularFireModule],
+  exports: [],
   providers: [{ provide: LOCALE_ID, useValue: 'pt' }],
   bootstrap: [AppComponent],
 })
