ad5e8cd1648761cfc9b74c26fcf61fbefb048be6
Giselle Barbosa <g88barbosa@gmail.com>
2023-07-13 20:38:31 -0300

    chore: firebase removed

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,24 +1,3 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
   production: false,
-  firebase: {
-    apiKey: 'AIzaSyAPzboGBtJJPiuXQj9MyoFSO7H6U7aOARw',
-    authDomain: 'finance-angular-8ef80.firebaseapp.com',
-    projectId: 'finance-angular-8ef80',
-    storageBucket: 'finance-angular-8ef80.appspot.com',
-    messagingSenderId: '73934019639',
-    appId: '1:73934019639:web:b5875eaaa4349124b4b3b2',
-  },
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
